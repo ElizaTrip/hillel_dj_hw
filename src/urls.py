73b448833e3893273c0651d4c8a84e438b88d903
Liza T <eliza.tripolskaya21@gmail.com>
2022-04-03 19:05:40 +0300

    Added template

--- src/urls.py
+++ src/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from app.views import index
+from app.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', index),
+    path('home/', home),
 ]
