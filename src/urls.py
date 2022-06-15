13db559bfc71a5b16e84690c755abee5624c363e
Liza T <eliza.tripolskaya21@gmail.com>
2022-06-15 16:14:05 +0300

    ФAdded swagger

--- src/urls.py
+++ src/urls.py
@@ -14,22 +14,35 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from rest_framework import routers
+from django.urls import include, path, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
-from app.views import home, email_send, sign_in, logging_out, register, activate
-from app.views import SubjectSelect, SubjectUpdate
-from app.views import TeacherSelect, TeacherUpdate
-from app.views import StudentSelect, StudentDetail
-from app.views import PersonViewSet, GroupViewSet, SubjectViewSet
+from app.router import router
+from app.views import (StudentDetail, StudentSelect, SubjectSelect,
+                       SubjectUpdate, TeacherSelect, TeacherUpdate, activate,
+                       email_send, home, logging_out, register, sign_in)
 
 
-router = routers.DefaultRouter()
-router.register(r'person', PersonViewSet)
-router.register(r'group', GroupViewSet)
-router.register(r'subject', SubjectViewSet)
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
     path('admin/', admin.site.urls),
     path('home/', home),
     path('email_send', email_send),
