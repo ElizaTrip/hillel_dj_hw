7b22ea7493db7099a86c63c8724a39ef257afa22
Liza T <eliza.tripolskaya21@gmail.com>
2022-07-26 17:10:00 +0300

    Added tests for API endpoints.

--- src/app/router.py
+++ src/app/router.py
@@ -3,6 +3,6 @@ from rest_framework import routers
 from app.views import PersonViewSet, GroupViewSet, SubjectViewSet
 
 router = routers.DefaultRouter()
-router.register(r'person', PersonViewSet)
-router.register(r'group', GroupViewSet)
-router.register(r'subject', SubjectViewSet)
+router.register(r'person', PersonViewSet, basename='person')
+router.register(r'group', GroupViewSet, basename='group')
+router.register(r'subject', SubjectViewSet, basename='subject')
