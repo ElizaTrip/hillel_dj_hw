92016c2603aed490c1a465fc54928ba43c76ed77
Liza T <eliza.tripolskaya21@gmail.com>
2022-07-28 17:27:35 +0300

    Added Dockerfile

--- src/settings/settings.py
+++ src/settings/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'some-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1']
 
 LOGIN_URL = '/login'
 LOGOUT_URL = '/logout'
