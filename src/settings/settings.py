55687efb4a300a9084593d1d7f0f7534469306ca
Liza T <eliza.tripolskaya21@gmail.com>
2022-07-30 16:31:59 +0300

    Added docker-compose

--- src/settings/settings.py
+++ src/settings/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
-from dotenv import load_dotenv
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-load_dotenv(dotenv_path=BASE_DIR / '../.env')
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'some-secret-key')
 
@@ -106,8 +103,12 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'example',
+        'HOST': 'postgres',
+        'PORT': '5432',
     }
 }
 
